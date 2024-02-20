84683c8c3711186216d29fed1d97b7593d987236
samer <sameradel789@gmail.com>
2024-02-20 08:33:47 +0200

    remove comments in build file

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 # Build the project
-echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
-
-echo "Make migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
-
-echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
