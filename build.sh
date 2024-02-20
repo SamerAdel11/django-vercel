3b7b6b03123f33da415a84faf1f37261d3751a17
samer <sameradel789@gmail.com>
2024-02-20 08:18:55 +0200

    added '--no input' in build file

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
 echo "Make migration..."
-python3.9 manage.py makemigrations 
-python3.9 manage.py migrate 
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
